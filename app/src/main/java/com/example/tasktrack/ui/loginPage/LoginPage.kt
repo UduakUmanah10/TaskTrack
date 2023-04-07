d69d42b41fb79e166f4f5a96eeed6ea6c09f9e15
uduak <uduakumanah10@gmail.com>
2023-04-07 14:55:22 +0100

    adding business logic of login

--- app/src/main/java/com/example/tasktrack/ui/loginPage/LoginPage.kt
+++ app/src/main/java/com/example/tasktrack/ui/loginPage/LoginPage.kt
@@ -3,29 +3,40 @@ import android.content.res.Configuration
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.tasktrack.R
 import com.example.tasktrack.ui.components.LoginAnimation
 import com.example.tasktrack.ui.components.PrimaryButton
 import com.example.tasktrack.ui.components.TrackAppTextField
+import com.example.tasktrack.ui.core.VerticalSpacer
 import com.example.tasktrack.ui.theme.TaskTrackTheme
 
 /**
  * This is a composable that maintains the the entire login Screen.
  * @param[viewState] the current state of what your screen renders
- * @param[]
- */
+ * @param[onLoginClicked] a call back that is invoked when the user clicks the login screen
+ * @param[onUserNameChanged]a call back that is invoked when the text field of user name changes
+ * @param[onSignupClicked] a call back that is invoked when the the sign ip button is clicked
+ * @param[onPasswordChanged] a call back that is invoked when there is changes in the pass word text field
+ * */
 
 @Composable
 fun LoginPage(
-    viewState: LoginViewState
+    viewState: LoginViewState,
+    onUserNameChanged: (String) -> Unit,
+    onPasswordChanged: (String) -> Unit,
+    onLoginClicked: () -> Unit,
+    onSignupClicked: () -> Unit
+
 ) {
     Scaffold(
         backgroundColor = MaterialTheme.colors.background
@@ -34,24 +45,79 @@ fun LoginPage(
         Column(
             modifier = Modifier
                 .padding(padding)
-                .padding(24.dp)
+                .padding(dimensionResource(id = R.dimen.screen_padding))
                 .fillMaxSize(),
 
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Spacer(modifier = Modifier.weight(1F))
+
             LoginAnimation()
+
             Spacer(modifier = Modifier.weight(1F))
 
-            TrackAppTextField(text = "", onTextChanged = {}, labelText = "Email")
-            Spacer(modifier = Modifier.height(12.dp))
-            TrackAppTextField(text = "", onTextChanged = {}, labelText = "PassWord")
-            Spacer(modifier = Modifier.height(48.dp))
-            PrimaryButton(text = "Log In", Onclick = { /*TODO*/ })
-            Spacer(modifier = Modifier.height(12.dp))
-            PrimaryButton(text = "Sign Up", Onclick = { /*TODO*/ })
+            Email(
+                text = viewState.userName,
+                onEmailTextChanged = onUserNameChanged
+
+            )
+
+            VerticalSpacer(height = 12.dp)
+
+            Password(
+                viewState.password,
+                onPasswordTextChanged = onPasswordChanged
+            )
+
+            VerticalSpacer(height = 48.dp)
+
+            LoginButton(onClick = onLoginClicked)
+
+            VerticalSpacer(height = 12.dp)
+
+            SignUpButton(onSignupClicked = onSignupClicked)
         }
     }
+}
+
+@Composable
+private fun Password(
+    text: String,
+    onPasswordTextChanged: (String) -> Unit
+) {
+    TrackAppTextField(
+        text = text,
+        onTextChanged = onPasswordTextChanged,
+        labelText = stringResource(id = R.string.Password)
+    )
+}
+
+@Composable
+private fun SignUpButton(onSignupClicked: () -> Unit) {
+    PrimaryButton(
+        text = stringResource(id = R.string.sign_up),
+        Onclick = onSignupClicked
+    )
+}
+
+@Composable
+private fun LoginButton(onClick: () -> Unit) {
+    PrimaryButton(
+        text = stringResource(id = R.string.Log_in),
+        Onclick = onClick
+    )
+}
+
+@Composable
+private fun Email(
+    text: String,
+    onEmailTextChanged: (String) -> Unit
+) {
+    TrackAppTextField(
+        text = text,
+        onTextChanged = onEmailTextChanged,
+        labelText = stringResource(id = R.string.Email)
+    )
 }
 
 @Preview(
@@ -67,6 +133,10 @@ fun LoginPage(
 private fun LoginPreview() {
     val viewState = LoginViewState("", "")
     TaskTrackTheme {
-        LoginPage(viewState = viewState)
+        LoginPage(
+            viewState = viewState,
+            {}, {}, {},
+            {}
+        )
     }
 }
