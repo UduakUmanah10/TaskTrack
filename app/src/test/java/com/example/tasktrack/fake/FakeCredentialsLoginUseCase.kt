7fa1364010c521ab25a6b772169828390d57f5f2
uduak <uduakumanah10@gmail.com>
2023-04-15 21:01:57 +0100

    implented view model

---
+++ app/src/test/java/com/example/tasktrack/fake/FakeCredentialsLoginUseCase.kt
@@ -0,0 +1,20 @@
+package com.example.tasktrack.fake
+
+import CredentialLoginUseCase
+import com.example.tasktrack.login.domain.Credentials
+import com.example.tasktrack.login.domain.model.LoginResults
+import io.mockk.coEvery
+import io.mockk.mockk
+
+class FakeCredentialsLoginUseCase {
+    val mock: CredentialLoginUseCase = mockk()
+
+    fun mockLoginresult(
+        credentials: Credentials,
+        result: LoginResults
+    ) {
+        coEvery {
+            mock(credentials)
+        } returns result
+    }
+}
