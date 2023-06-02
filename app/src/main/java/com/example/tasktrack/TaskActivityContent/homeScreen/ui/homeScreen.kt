14a45c363dcbfd46f54acc4a08229d15f62c1ebb
uduak umanah <uduakumanah10@gmail.com>
2023-06-02 18:01:08 +0100

    update

---
+++ app/src/main/java/com/example/tasktrack/TaskActivityContent/homeScreen/ui/homeScreen.kt
@@ -0,0 +1,11 @@
+package com.example.tasktrack.TaskActivityContent.homeScreen.ui
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@Composable
+fun HomeScreen(navController: NavController, modifier: Modifier = Modifier) {
+    Text( text= "HomeScreen")
+}
